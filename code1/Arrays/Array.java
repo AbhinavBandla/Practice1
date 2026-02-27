7abbc502b4cd590f87dc7b3e4a7f67096a615ef3
unknown <Abhinavbandlaece@gmail.com>
2026-02-27 22:19:06 +0530

    Array

---
+++ code1/Arrays/Array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class array{
+    public static void main(String[] args){
+        // array = is Collection of values of same type of data or which can more than 2 values
+        // array are reference type to acces the data we have give the memory location 
+        // if we try to access the data that is not present error : ArrayIndexOutofBound
+        // for each loop 
+        // import Arrays - to access the built in functions
+        String[] things = { "Cars","Bikes","Fruits","Chocolates"};
+
+        System.out.println(things[1]);
+        System.out.println("Length of an array is " + things.length);
+
+       //(error) System.out.println(things[5]);
+
+
+
+
+    }
+}
