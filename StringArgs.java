276df2c925c502964835014a1cfec16a4b703faf
unknown <Abhinavbandlaece@gmail.com>
2026-03-23 08:32:06 +0530

    String[] Args

---
+++ StringArgs.java
@@ -0,0 +1,12 @@
+
+//String[] args is an array of Strings that stores command-line arguments passed -
+// - from outside the program through the terminal when the program starts.
+
+
+class TeaShop {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
