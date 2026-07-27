39db4bc19035f2131ad2ded30f13788bbc58b01c
unknown <Abhinavbandlaece@gmail.com>
2026-07-27 20:54:42 +0530

    Encapsulation Demo

---
+++ OOPJ/Encapsulation/EnCapDemo.java
@@ -0,0 +1,29 @@
+class enCap{
+    public static void main(String[] args){
+        /*
+        Encapsulation means wrapping data (variables) and the methods that operate on that data into one class,
+        and controlling how the data is accessed or modified.
+
+        "In the class we have data & method anyone can access it and change it but it is harmful. Encapsulation will decide who will enter or not."
+        "Encapsulation will decide who will enter or not."
+
+        Encapsulation = Bundle + Control
+
+        Where:
+
+        Bundle → Keep data and methods together.
+        Control → Decide how the data is accessed or changed.
+
+        DATA - Hiding :- 
+        Data Hiding means hiding important data from direct access so that it cannot be changed by anyone from outside the class.
+
+        Private KeyWord : 
+        private is an access modifier that hides a variable or method from direct access outside the class.
+
+        In simple English:
+        private means "Only this class can directly use this member."
+        private does not hide data from the class itself. It hides data from other classes.
+        
+        */
+    }
+}
